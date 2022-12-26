9fda883cbe98442e74428f41892b710315ef34be
Mars Smith <119779217+MarsRising@users.noreply.github.com>
2022-12-25 17:44:31 -0700

    Update cc_dicegame.py

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -2,16 +2,22 @@ import random
 
 #game initialized
 high_score = 0
-print('Current High Score:', high_score)
-print('1) Roll Dice')
-print('2) Leave Game')
-dice_game=input('Enter your choice:')
+
 #game works
-def dice_game():
-    if dice_game=='1'
-        di=random.randint(1, 6)
-        print('You roll a ...', di)
-        high_score=high_score+di
-
-
-dice_game()
+while True:
+    print('Current High Score:', high score, \n '1)Roll Dice', \n, '2) Leave Game'\n)
+    dice_game=input('Enter your Choice:')
+        if dice_game=='1':
+            thrown=random.randint(1, 6)
+            print('You roll a ...', str(thrown), \n)
+            throwntwo=random.randint(1,6)
+            print('You roll a ...', str(throwntwo), \n)
+            roll= throw+throwtwo
+            print('You have rolled a total of:', roll)
+                if roll > high_score
+                    high_score=roll
+                    print(New High Score!)
+        elif dice_game=='2':
+            quit()
+        else:
+            print("Unknown Response")
