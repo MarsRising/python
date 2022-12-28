902017042da7b3b83f21cc95575d254150a8d524
Margot Smith <itsyadame@gmail.com>
2022-12-28 09:57:57 -0700

    new_folder

---
+++ week2/__pycache__/trial_dicegame.py
@@ -0,0 +1,23 @@
+import random
+
+#game initialized
+high_score = 0
+
+#game works
+while True:
+    print('Current High Score:', high_score, '\n1)Roll Dice\n' + '2)Leave Game\n')
+    dice_game=input('Enter your Choice:')
+    if dice_game=='1':
+        thrown=random.randint(1, 6)
+        print('\nYou roll a ...', str(thrown))
+        throwntwo=random.randint(1,6)
+        print('\nYou roll a ...', str(throwntwo))
+        roll= thrown+throwntwo
+        print('\nYou have rolled a total of:', roll)
+        if roll > high_score:
+            high_score=roll
+            print('\nNew High Score!\n')
+    elif dice_game=='2':
+        quit()
+    else:
+        print("Unknown Response")
